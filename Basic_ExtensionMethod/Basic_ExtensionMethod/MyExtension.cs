5ef4d52c19190791e22aba33895f85452988274f
haingv134 <haivann2000@gmail.com>
2021-09-22 22:32:49 +0700

    Extension Method - Add more custu method for specfic class

---
+++ Basic_ExtensionMethod/Basic_ExtensionMethod/MyExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_ExtensionMethod
+{
+    public static class MyExtension
+    {
+        public static double BinhPhuong(this double x) => x * x;
+        public static double CanBacHai(this double x) => Math.Sqrt(x);
+    }
+}
---
+++ Basic_ExtensionMethod/Basic_ExtensionMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+// extension method
+// lop tinh static
+namespace Basic_ExtensionMethod
+{
+    // mo rong them method Print cho String : this string
+    internal static class Ab
+    {
+        public static void Print(this string s, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(s);
+            Console.ResetColor();
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string s = "xin chao cac ban";
+            s.Print(ConsoleColor.Green);
+            s.Print(ConsoleColor.Red);
+            "Xin".Print(ConsoleColor.Gray);
+
+            double a = 2.5;
+            Console.WriteLine($"binh phuongL {a.BinhPhuong()}, can bac hai: {a.CanBacHai()}");
+        }
+    }
+}
