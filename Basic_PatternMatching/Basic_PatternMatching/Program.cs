366d2adb84b50e56666258daaacc967c9c1e9869
haingv134 <haivann2000@gmail.com>
2021-09-22 23:59:12 +0700

    Pattern

---
+++ Basic_PatternMatching/Basic_PatternMatching/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Basic_PatternMatching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? maybe = 12;
+
+            if (maybe is int number)
+            {
+                Console.WriteLine($"The nullable int 'maybe' has the value {number}");
+            }
+            else
+            {
+                Console.WriteLine("The nullable int 'maybe' doesn't hold a value");
+            }
+        }
+    }
+}
