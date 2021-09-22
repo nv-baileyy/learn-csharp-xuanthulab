d2a92b7465e094e3b5ac7fc34129507bf3929143
haingv134 <haivann2000@gmail.com>
2021-09-22 22:30:43 +0700

    Basic Exception - Custom Exception

---
+++ Basic_ExceptionHandling/Basic_ExceptionHandling/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Basic_ExceptionHandling
+{
+    internal class Program
+    {
+        // custom exception, inheren from base class Exception
+        // When use, it'll call Contructor of Exception
+        private class NameEmptyException : Exception
+        {
+            public NameEmptyException() : base("Ten phai khac rong")
+            {
+            }
+        }
+
+        private static void Register(string name, int old)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new NameEmptyException();
+            }
+            Console.WriteLine($"Xin chao {name} ({old})");
+        }
+
+        private static void Main(string[] args)
+        {
+            try
+            {
+                Register("", 12);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + " \n " + e.GetType().Name);
+            }
+        }
+    }
+}
