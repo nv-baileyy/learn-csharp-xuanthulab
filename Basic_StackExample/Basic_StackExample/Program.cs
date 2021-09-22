eb89c3f4ec584c79b8e35a5b82d9a59fb4acc120
haingv134 <haivann2000@gmail.com>
2021-09-22 23:58:19 +0700

    Stack

---
+++ Basic_StackExample/Basic_StackExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Basic_StackExample
+{
+    public class Stack<T>
+    {
+        private Entry _top;
+
+        public void Push(T data)
+        {
+            _top = new Entry(_top, data);
+        }
+
+        public T Pop()
+        {
+            if (_top == null)
+                throw new Exception("Stack is null");
+            T value = _top.Data;
+            _top = _top.Pre;
+            return value;
+        }
+
+        private class Entry
+        {
+            public Entry Pre { get; set; }
+            public T Data { get; set; }
+
+            public Entry(Entry pre, T data) => (Pre, Data) = (pre, data);
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+        }
+    }
+}
