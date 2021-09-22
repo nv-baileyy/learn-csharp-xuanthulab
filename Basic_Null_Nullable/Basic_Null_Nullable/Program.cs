a3ef34330fe3c096a0051776b786a8b165dde83c
haingv134 <haivann2000@gmail.com>
2021-09-22 23:59:53 +0700

    Execute with ?, nullable variable

---
+++ Basic_Null_Nullable/Basic_Null_Nullable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Basic_Null_Nullable
+{
+    class Program
+    {
+        class Abc
+        {
+            public void Xinchao() => Console.WriteLine("hello world");
+        }
+        static void Main(string[] args)
+        {
+            Abc abc = new Abc();
+            abc?.Xinchao(); // = if (a!=null) a.xinchao()
+            int? x; // allow x accept null value
+            x = null;
+            x = 10;            
+            if (x.HasValue)
+            {
+                Console.WriteLine($"x has value: {x}");
+            }
+        }
+    }
+}
