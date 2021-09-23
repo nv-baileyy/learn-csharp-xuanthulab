edc9f50d0901e5c789c1e05646e4130c049663b9
haingv134 <haivann2000@gmail.com>
2021-09-23 23:47:27 +0700

    A few taghelper and htmlhelper

---
+++ Razor-TagHelper-HtmlHelper/Razor-TagHelper-HtmlHelper/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Razor_TagHelper_HtmlHelper.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [DisplayName("Ten nguoi dung")]
+        public string UserName { get; set; } = "nv.baileyy";
+         
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
